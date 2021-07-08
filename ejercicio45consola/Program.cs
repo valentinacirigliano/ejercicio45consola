c3fc96293fd09a08f6e225833105f8a93710db31
valentinacirigliano <ciriglianovalentina2@gmail.com>
2021-07-08 15:15:33 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio45consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio45consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "SUMA DE LOS PRIMEROS 5 NÚMEROS";
+            int  numeroIngresado, suma=0;
+            for (int contador = 1; contador < 6; contador++)
+            {
+                Console.Write($"Ingrese el {contador}° número: ");
+                numeroIngresado = Convert.ToInt32(Console.ReadLine());
+                suma += numeroIngresado;
+            }
+            Console.WriteLine($"Valor final de la suma:  {suma}");
+            Console.ReadLine();
+        }
+    }
+}
